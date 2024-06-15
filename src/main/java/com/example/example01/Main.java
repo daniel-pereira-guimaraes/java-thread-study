3cd818c49f38f9de44810359d989f817dae9c692
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2024-06-15 10:02:44 -0400

    Example 01 - Two slow processing

---
+++ src/main/java/com/example/example01/Main.java
@@ -0,0 +1,36 @@
+package com.example.example01;
+
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        var runnable = new Runnable() {
+            @Override
+            public void run() {
+                slowProcessing();
+            }
+        };
+
+        var thread = new Thread(runnable);
+
+        thread.start();
+        slowProcessing();
+    }
+
+    private static void slowProcessing() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(Thread.currentThread().getName());
+            randomDelay();
+        }
+    }
+
+    private static void randomDelay() {
+        try {
+            Thread.sleep(new Random().nextInt(1000));
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
