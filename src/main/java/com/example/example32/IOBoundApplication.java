ee752718a02f4e3954f1c056db82af9f908fee8a
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2024-06-30 12:46:44 -0400

    Example 32 - Running many tasks using a dynamic thread pool

---
+++ src/main/java/com/example/example32/IOBoundApplication.java
@@ -0,0 +1,42 @@
+package com.example.example32;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class IOBoundApplication {
+    private static final int NUMBER_OF_TASKS = 1000;
+
+    public static void main(String[] args) {
+        System.out.printf("Running %d tasks\n", NUMBER_OF_TASKS);
+        long start = System.currentTimeMillis();
+        performTasks();
+        System.out.printf("Tasks took %dms to complete\n", System.currentTimeMillis() - start);
+    }
+
+    private static void performTasks() {
+        var executorService = Executors.newCachedThreadPool();
+        try {
+            for (int i = 0; i < NUMBER_OF_TASKS; i++) {
+                executorService.submit(IOBoundApplication::simulateLongBlockingIO);
+            }
+        } finally {
+            executorService.shutdown();
+            while (!executorService.isTerminated()) {
+                try {
+                    executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+
+    private static void simulateLongBlockingIO() {
+        System.out.println("Executing a blocking task from thread: " + Thread.currentThread());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
